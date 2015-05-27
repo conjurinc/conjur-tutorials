2da413737e738563ac8f59b62e9deda032d77d59
Hleb Rubanau <g.rubanau@gmail.com>
2015-05-27 22:20:34 +0300

    Change policy

--- ldap-test/ldap.rb
+++ ldap-test/ldap.rb
@@ -1,21 +1,18 @@
 policy "ldap-#{ENV['CONJUR_POLICY_VERSION']}" do
 
-     host = host "tomcat"
-
      josh = user "josh"
      kevin = user "kevin"
 
+
      devs = group "devs" do
           add_member josh
-          add_member host
      end
      ops  = group "ops" do
           add_member kevin
-          add_member host
      end
 
-
-     devs.can "execute",host
-
-     ops.can "execute",host
+     layer = layer "tomcats" do
+       add_member "use_host", devs
+       add_member "use_host", ops
+     end
 end
